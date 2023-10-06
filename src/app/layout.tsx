bc584de7e78aff36cdb14be7ad0c4d72e9e2a4f9
ozy <ozyftw17@gmail.com>
2023-10-06 17:53:14 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { cn  } from '@/lib/utils'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+          className={cn(
+            'min-h-screen font-sans antialiased grainy',
+            inter.className
+          )}>
+            {children}
+              
+            </body>
     </html>
   )
 }
---
+++ src/app/lib/utils.ts
@@ -0,0 +1,10 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const cn = (...inputs:ClassValue[]) => {
+    return ( 
+        twMerge(clsx(inputs))
+     );
+}
+ 
+export default cn;
