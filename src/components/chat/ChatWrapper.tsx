07b91bd09b2cd34d2e6be2b1635a2931b76378d8
ozy <ozyftw17@gmail.com>
2023-10-12 23:50:08 +0530

    loading states on pdf render

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -3,7 +3,7 @@
 import Messages from "./Messages";
 import ChatInput from "./ChatInput";
 import { trpc } from "@/app/_trpc/client";
-import { Loader2 } from "lucide-react";
+import { Loader2, XCircle } from "lucide-react";
 
 interface ChatWrapperProps {
   fileId: string;
@@ -36,12 +36,42 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
       </div>
     );
 
+  if (data?.status === "PROCESSING")
+    return (
+      <div className="relative min-h-full bg-stone-50 flex divide-y  divide-stone-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex justify-center items-center flex-col mb-28">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 text-pink-500 animate-spin" />
+            <h3 className="font-semibold text-xl ">processing PDF...</h3>
+            <p className="text-stone-500 text-sm">this won&apos;t take long.</p>
+          </div>
+        </div>
+        <ChatInput />
+      </div>
+    );
+
+  if (data?.status === "FAILED")
+    return (
+      <div className="relative min-h-full bg-stone-50 flex divide-y  divide-stone-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex justify-center items-center flex-col mb-28">
+          <div className="flex flex-col items-center gap-2">
+            <XCircle className="h-8 w-8 text-red-700 animate-spin" />
+            <h3 className="font-semibold text-xl ">File size too big</h3>
+            <p className="text-stone-500 text-sm">
+              try uploading a smaller file
+            </p>
+          </div>
+        </div>
+        <ChatInput />
+      </div>
+    );
+
   return (
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-stone-200 flex-col justify-between gap-2">
       <div className="flex-1 justify-between flex flex-col mb-28">
         <Messages />
       </div>
-      <ChatInput />
+      <ChatInput isDisabled />
     </div>
   );
 };
