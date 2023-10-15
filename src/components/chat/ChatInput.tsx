81863e31a85e89845b8a70c12039ff7aadc024f2
ozy <ozyftw17@gmail.com>
2023-10-15 22:33:58 +0530

    changed env

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -14,7 +14,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4 ">
             <div className="relative">
@@ -40,7 +40,6 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
               <Button
                 disabled={isLoading || isDisabled}
                 className="absolute bottom-1.5 right-[8px]"
-                type="submit"
                 onClick={() => {
                   addMessage();
 
@@ -52,7 +51,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
