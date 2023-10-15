4441fe00d51223f2d2c2aa0afb98bba7a3ddac70
ozy <ozyftw17@gmail.com>
2023-10-16 00:25:43 +0530

    kinde 🤬

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,27 +5,20 @@ import { Style } from "util";
 import Dashboard from "@/components/Dashboard";
 
 const Page = async () => {
-    const{getUser} = getKindeServerSession()
+  const { getUser } = getKindeServerSession();
 
-    const user = getUser()
+  const user = getUser();
 
-    if(!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-    const dbUser = await db.user.findFirst({
-        where:{
-            id: user.id,
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
 
-        }
-    })
-    if(!dbUser) redirect('/auth-callback?origin=dashboard')
+  return <Dashboard />;
+};
 
-
-
-    return (
-       
-        
-        <Dashboard/>
-    )
-}
- 
 export default Page;
