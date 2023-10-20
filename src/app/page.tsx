b1ac4ae81293bd4cd1d60ccfcd358fac9300a418
ozy <ozyftw17@gmail.com>
2023-10-20 22:56:45 +0530

    final tweaks🥳

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,9 @@ export default function Home() {
             <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
               <span className="text-sm font-medium text-pink-600">step 1</span>
               <span className="text-xl font-semibold">sign up</span>
-              <span className="mt-2 text-zinc-700">or log in with github</span>
+              <span className="mt-2 text-zinc-700">
+                this service is passwordless
+              </span>
             </div>
           </li>
           <li className="md:flex-1">
